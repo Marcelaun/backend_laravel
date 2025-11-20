9aec8a5c2054593efde58566ba98d31a7e1fc621
Marcelaun <cellort567@gmail.com>
2025-11-20 05:45:28 -0300

    mudando configs do cors e do session.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://visus-ai-frontend.vercel.app'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://visus-ai-frontend.vercel.app', 'https://visus-ai-frontend.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
