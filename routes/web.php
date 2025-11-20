0f907968c7daea37d20a55195e4690ff66370130
Marcelaun <cellort567@gmail.com>
2025-11-20 04:03:28 -0300

    Correção de rota duplicada

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('welcome'); // Ou return response()->json(['status' => 'API Online']);
 });
-
-require __DIR__.'/auth.php';
